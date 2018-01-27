631e217288c818c8930f246bfb124282ff00d851
Justin Flick <justinjayflick@gmail.com>
2018-01-26 19:51:12 -0800

    added voice_assets

--- voice.py
+++ voice.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
+from controller
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
 @ask.launch
-def start_session()
+def start_session():
     welcome_text = render_template('')
     welcome_re_text = render_template('welcome_re')
 
     return question(welcome_text).reprompt(welcome_re_text)
 
+if __name__ == '__main__':
+    app.run(debug=True)
