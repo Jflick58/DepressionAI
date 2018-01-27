e084c631584d65c49b25589312381e6cce12b6a0
Justin Flick <justinjayflick@gmail.com>
2018-01-26 20:32:38 -0800

    added default intents

--- voice.py
+++ voice.py
@@ -7,12 +7,71 @@ ask = Ask(app, '/')
 
 @ask.launch
 def start_session():
-    welcome_text = random_welcome()
+    welcome_text = welcome()
     welcome_re_text = re()
 
     return question(welcome_text).reprompt(welcome_re_text)
 
 """These functions handle intent logic for the voice interface. """
+
+@ask.intent('AMAZON.StopIntent')
+def handle_stop():
+    """
+    (STATEMENT) Handles the 'stop' built-in intention.
+    """
+    farewell_text = render_template('stop_bye')
+    return statement(farewell_text)
+
+
+@ask.intent('AMAZON.CancelIntent')
+def handle_cancel():
+    """
+    (STATEMENT) Handles the 'cancel' built-in intention.
+    """
+    farewell_text = render_template('cancel_bye')
+    return statement(farewell_text)
+
+
+@ask.intent('AMAZON.HelpIntent')
+def handle_help():
+    """
+    (QUESTION) Handles the 'help' built-in intention.
+
+    You can provide context-specific help here by rendering templates conditional on the help referrer.
+    """
+
+    help_text = render_template('help_text')
+    return question(help_text)
+
+
+@ask.intent('AMAZON.NoIntent')
+def handle_no():
+    """
+    (?) Handles the 'no' built-in intention.
+    """
+    pass
+
+@ask.intent('AMAZON.YesIntent')
+def handle_yes():
+    """
+    (?) Handles the 'yes'  built-in intention.
+    """
+    pass
+
+
+@ask.intent('AMAZON.PreviousIntent')
+def handle_back():
+    """
+    (?) Handles the 'go back!'  built-in intention.
+    """
+    pass
+
+@ask.intent('AMAZON.StartOverIntent')
+def start_over():
+    """
+    (QUESTION) Handles the 'start over!'  built-in intention.
+    """
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
